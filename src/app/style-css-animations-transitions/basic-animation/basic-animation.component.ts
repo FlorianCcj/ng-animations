a042935e95b88078db01eedcb2b5e5c4af90ffc5
florianccj <florianccj@gmail.com>
2018-05-01 13:54:13 +0200

    [*][basic animation] tuto ok, begin project

--- src/app/style-css-animations-transitions/basic-animation/basic-animation.component.ts
+++ src/app/style-css-animations-transitions/basic-animation/basic-animation.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basic-animation',
   templateUrl: './basic-animation.component.html',
   styleUrls: ['./basic-animation.component.scss']
 })
-export class BasicAnimationComponent implements OnInit {
+export class BasicAnimationComponent {
+
+  divClicked = false;
+  doAnimate = false;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
