dee93cd20e9aa44b05e1a11fee42d02b2e5e0fe6
florianccj <florianccj@gmail.com>
2018-05-01 12:21:04 +0200

    [*][css] css transition ok

--- src/app/style-css-animations-transitions/complexe-transition/complexe-transition.component.ts
+++ src/app/style-css-animations-transitions/complexe-transition/complexe-transition.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-complexe-transition',
   templateUrl: './complexe-transition.component.html',
   styleUrls: ['./complexe-transition.component.scss']
 })
-export class ComplexeTransitionComponent implements OnInit {
+export class ComplexeTransitionComponent {
+
+  divClicked = false;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- src/app/style-css-animations-transitions/simple-transition/simple-transition.component.ts
+++ src/app/style-css-animations-transitions/simple-transition/simple-transition.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-simple-transition',
   templateUrl: './simple-transition.component.html',
   styleUrls: ['./simple-transition.component.scss']
 })
-export class SimpleTransitionComponent implements OnInit {
+export class SimpleTransitionComponent {
+
+  divClicked = false;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
