de280e238ffbdbe4c384c0b7c5c9d4696d1bec37
florianccj <florianccj@gmail.com>
2018-05-01 16:03:34 +0200

    [*][ng anim] init proj

--- src/app/style-ng-animations/style-ng-animations.module.ts
+++ src/app/style-ng-animations/style-ng-animations.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProjetNgAnimationsComponent } from './projet-ng-animations/projet-ng-animations.component';
+import { NewProjectComponent } from './projet-ng-animations/new-project/new-project.component';
+import { ProjectComponent } from './projet-ng-animations/project/project.component';
+import { ProjectsComponent } from './projet-ng-animations/projects/projects.component';
+import { UsersComponent } from './projet-ng-animations/users/users.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProjetNgAnimationsComponent, NewProjectComponent, ProjectComponent, ProjectsComponent, UsersComponent]
 })
 export class StyleNgAnimationsModule { }
