d625a676e2f93b313300d3bb8ddfef1a229cfcfd
florianccj <florianccj@gmail.com>
2018-05-08 12:31:18 +0200

    [*][init project]

---
+++ src/app/style-ng-animations-advanced/project-ng-animation-advance/project-ng-animation-advance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-ng-animation-advance',
+  templateUrl: './project-ng-animation-advance.component.html',
+  styleUrls: ['./project-ng-animation-advance.component.scss']
+})
+export class ProjectNgAnimationAdvanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
