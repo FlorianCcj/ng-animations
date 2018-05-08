e3d52aa00d2ee7b6becbd5f1b126054a42886d04
florianccj <florianccj@gmail.com>
2018-05-08 12:30:42 +0200

    [*][tuto anim] end

--- src/app/style-ng-animations-advanced/style-ng-animations-advanced.module.ts
+++ src/app/style-ng-animations-advanced/style-ng-animations-advanced.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgAnimationsAdvancedIndexComponent } from './ng-animations-advanced-index/ng-animations-advanced-index.component';
-import { SynamicDimentionalPropComponent } from './synamic-dimentional-prop/synamic-dimentional-prop.component';
 import { MetaStateComponent } from './meta-state/meta-state.component';
+import { DynamicDimentionalPropComponent } from './dynamic-dimentional-prop/dynamic-dimentional-prop.component';
+import { ListAnimationsComponent } from './list-animations/list-animations.component';
+import { TimingFunctionComponent } from './timing-function/timing-function.component';
+import { KeyframesComponent } from './keyframes/keyframes.component';
+import { AnimationEventComponent } from './animation-event/animation-event.component';
+import { ProjectNgAnimationAdvanceComponent } from './project-ng-animation-advance/project-ng-animation-advance.component';
 
 @NgModule({
   imports: [
@@ -10,8 +15,13 @@ import { MetaStateComponent } from './meta-state/meta-state.component';
   ],
   declarations: [
     NgAnimationsAdvancedIndexComponent,
-    SynamicDimentionalPropComponent,
-    MetaStateComponent
+    MetaStateComponent,
+    DynamicDimentionalPropComponent,
+    ListAnimationsComponent,
+    TimingFunctionComponent,
+    KeyframesComponent,
+    AnimationEventComponent,
+    ProjectNgAnimationAdvanceComponent
   ]
 })
 export class StyleNgAnimationsAdvancedModule { }
