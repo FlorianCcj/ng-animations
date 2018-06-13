a359b802184207c00cc3e6b3ed36f41953012493
florianccj <florianccj@gmail.com>
2018-06-13 22:41:25 +0200

    [+][layout] add some layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RouterModule, provideRoutes } from '@angular/router';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     FormsModule,
     HttpModule,
---
+++ src/app/core/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  go(path: any[], query?: object, extras?: NavigationExtras) {
+    this.router.navigate(path, { queryParams: query, ...extras })
+  }
+
+}
