29cf829f015486e28537b56668eb6c8600f926ca
florianccj <florianccj@gmail.com>
2018-06-30 19:26:43 +0200

    [*][style] organisation of paragraph

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,7 +4,6 @@ import { LayoutComponent } from './layout/layout.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
